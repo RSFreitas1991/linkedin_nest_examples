5158e2eb89a375a40d4e0c86eb06741123ec3d14
Raphael Freitas <raphael.soarescivil@gmail.com>
2025-07-05 17:25:11 -0300

    feat: console.log on listen app

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
-  console.log(`Application is running on: http://localhost:${port}`);
-  await app.listen(port);
+  await app.listen(port, () =>
+    console.log(`Server is running on port ${port}`),
+  );
 }
 bootstrap();
